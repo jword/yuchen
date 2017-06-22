695fc8a5311e0d421c6fe8ea349f2d73169daa13
jword <738594929@qq.com>
2017-06-22 18:28:31 +0800

    优化环境变量 	modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -10,7 +10,12 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
-error_reporting(E_ALL);
+$cfgvar = get_cfg_var('phaplus.env');
+$env    = !empty($cfgvar) ? $cfgvar : 'dev';
+define('APP_ENV', $env);
+if ($env = 'product') {
+    error_reporting(0);
+}
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
